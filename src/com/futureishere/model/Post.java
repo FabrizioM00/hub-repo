3c2906a90e78d17c2d479899429a48e27f3c6fa0
FabrizioM00 <116150549+FabrizioM00@users.noreply.github.com>
2023-02-20 23:16:42 +0100

    added id to post

--- src/com/futureishere/model/Post.java
+++ src/com/futureishere/model/Post.java
@@ -5,6 +5,7 @@ public class Post {
 	//titolo
 	private String title;
 	private String description;
+	private int id;
 	
 	public String getTitle() {
 		return title;
@@ -17,6 +18,12 @@ public class Post {
 	}
 	public void setDescription(String description) {
 		this.description = description;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
 	}
 	
 }
